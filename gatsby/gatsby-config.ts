cf55a0806e8ecb18ad8fa1c25948df4420450e2d
milky <listingslab@gmail.com>
2023-09-22 11:31:59 +0200

    Updates core Gatsby files

---
+++ gatsby/gatsby-config.ts
@@ -0,0 +1,75 @@
+import {GatsbyShape} from "./types"
+import type { GatsbyConfig } from "gatsby"
+import {goldlabelConfig} from ".//goldlabelConfig"
+
+const siteTitle = goldlabelConfig.siteTitle
+const siteDescription = goldlabelConfig.siteDescription
+const siteUrl = goldlabelConfig.siteUrl
+const siteIcon = goldlabelConfig.siteIcon
+const siteImage = goldlabelConfig.siteImage
+const siteTwitter = goldlabelConfig.siteTwitter
+const siteTheme = goldlabelConfig.siteTheme
+const siteKeywords = goldlabelConfig.siteKeywords
+const siteDefaultLocale = goldlabelConfig.siteDefaultLocale
+
+const siteMetadata: GatsbyShape =  {
+  siteTitle,
+  title: siteTitle,
+  siteUrl,
+  siteDescription,
+  description: siteDescription,
+  siteIcon,
+  siteImage,
+  image: siteImage,
+  siteKeywords,
+  siteTwitter,
+  twitterUsername: siteTwitter,
+  siteTheme,
+  siteDefaultLocale,
+}
+
+const config: GatsbyConfig = {
+  siteMetadata,
+  graphqlTypegen: false,
+  plugins: [
+    { resolve: 'gatsby-plugin-next-seo'},
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        start_url: `/`,
+        display: `standalone`,
+        name: siteTitle,
+        short_name: siteTitle,
+        icon: `./static${siteIcon}`,
+        background_color: siteTheme,
+        theme_color: siteTheme,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown`,
+      },
+      __key: "markdown"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ]
+}
+export default config
---
+++ gatsby/goldlabelConfig.ts
@@ -0,0 +1,40 @@
+import {ConfigShape} from "./types"
+
+export const goldlabelConfig: ConfigShape = {
+    siteUrl: "https://listingslab.com",
+    siteDefaultLocale: "en",
+    siteTitle: "Open Source",
+    siteDescription: "Fullstack JavaScript Since 1998",
+    siteImage: "/png/goldlabel.png",
+    siteIcon: "/svg/iOS.svg",
+    siteTwitter: "@machine_v4",
+    siteTheme: "#121212",
+    siteKeywords: "listingslab, goldlabel, opensource, fullstack, react, javascript, node, pwa, free, github",
+    themes: [
+        {
+            slug: "goldlabel",
+            primaryColor: "#C09F52",
+            secondaryColor: "#87703B",
+        },
+    ],
+    localeList: ["en", "mt", "de", "cn"],
+    localeMeta: {
+        en: {
+            default: "English",
+            localised: "Translate to English",
+        },
+        de: {
+            default: "Deutsch",
+            localised: "Ins Deutsche übersetzen",
+        },
+        cn: {
+            default: "简体中文",
+            localised: "翻译成简体中文",
+        },
+        mt: {
+            default: "Malti",
+            localised: "Ittraduci ghall-Malti",
+        },
+    },
+}
+
