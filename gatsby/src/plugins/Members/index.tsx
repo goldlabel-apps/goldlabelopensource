ab9330a32873c0baaa612b07f27712219013b88e
milky <listingslab@gmail.com>
2023-11-19 14:48:47 +0100

    Removes stale plugins
    Co-Authored-By: 魏臧 <weizang75@gmail.com>

--- gatsby/src/plugins/Members/index.tsx
+++
@@ -1,9 +0,0 @@
-import {membersForm} from "./membersForm"
-import MembersView from "./components/MembersView"
-import MemberForm from "./components/MemberForm"
-
-export {
-    membersForm,
-    MembersView,
-    MemberForm,
-}
