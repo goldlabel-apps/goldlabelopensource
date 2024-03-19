2349d82e73bc580b56e750c6cc70a358346b652f
Chris Dorward <listingslab@gmail.com>
2024-03-19 08:31:59 +0000

    4.3.3

--- gatsby/src/config.ts
+++ gatsby/src/config.ts
@@ -1,30 +1,31 @@
-export type GoldlabelConfig = {
+export type Config = {
     version: string
-    isomorphic: any
     siteUrl: string
-    siteIcon: string
-    siteRepo: string
-    siteAvatar: string
     siteTitle: string
     siteDescription: string
-    siteEmail: string
+    siteKeywords: string
     siteDisplayname: string
+    siteEmail: string
+    siteIcon: string
+    siteImage: string 
+    siteAvatar: string    
+    siteRepo: string
+    siteTheme: string
+    isomorphic: any
 }
 
-export const glConfig: any = {
+export const glConfig: Config = {
+    version: "4.3.3",
+    siteUrl: "https://free.goldlabel.pro",
     siteTitle: "Goldlabel",
-    version: "4.3.2",
+    siteDescription: "Free & Open Source on Github",
+    siteKeywords: "Goldlabel, Open Source, Free, Github",
     siteDisplayname: "AI",
     siteEmail: "ai@goldlabel.pro",
-    siteUrl: "https://free.goldlabel.pro",
     siteIcon: "/svg/iOS.svg",
     siteImage: "/webp/coupland/coupland.webp",
-    siteRepo: "https://github.com/listingslab-software/goldlabelopensource",
     siteAvatar: "https://free.goldlabel.pro/svg/iOS.svg",
-    siteDescription: "Free & Open Source on Github",
-    siteFacebook: "https://www.facebook.com/goldlabelapps",
-    siteKeywords: "listingslab, Goldlabel, Open Source, Free, Github",
-    siteTwitter: "@listingslab",
+    siteRepo: "https://github.com/listingslab-software/goldlabelopensource",
     siteTheme: "#FFFFFF",
     isomorphic: {
         forms: { enabled: false },
